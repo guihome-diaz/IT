a59861e0c03b3742327311f349dc4a9391c9da5c
Guillaume <guillaume@qin-diaz.com>
2020-08-29 20:59:48 +0200

    List photos files

--- wordpress/backup/backup_ngg_galleries.sh
+++ wordpress/backup/backup_ngg_galleries.sh
@@ -12,6 +12,7 @@
 
 ##### Global variables (they are populated by the functions)
 declare -a galleries
+photos_files=()
 
 # Get current IP @, do not change that line
 WORDPRESS_ROOT=$(pwd)
@@ -48,10 +49,11 @@ function getPhotosFiles() {
   #mapfile -d $'\0' photos_files < <(find ${WORDPRESS_ROOT}/wp-content/gallery/ -name "*.*_backup" -print0)
 
   # old bash (OVH server is v4.14 in 2020/09)
-  readarray -t photos_files < <(find ${WORDPRESS_ROOT}/wp-content/gallery/ -name "*.*_backup")
-#  while IFS= read -r -d '' photofile; do
-#    photos_files+=("${photofile}")
-#  done < <(find ${WORDPRESS_ROOT}/wp-content/gallery/ -name "*.*_backup" -print0)
+#  readarray -t photos_files < <(find ${WORDPRESS_ROOT}/wp-content/gallery/ -name "*.*_backup")
+
+  while IFS= read -r -d $'\0' photofile; do
+    photos_files+=("${photofile}")
+  done < <(find ${WORDPRESS_ROOT}/wp-content/gallery/ -name "*.*_backup" -print0)
 
   echo -e "    ${YELLOW}${#photos_files[@]} photos found${WHITE}"
   echo -e " "
@@ -68,7 +70,6 @@ echo -e " "
 
 # List files
 getPhotosFiles
-declare -a photos_files
 echo -e "    Files:"
 for gallery in "${photos_files[@]}"; do
   echo -e "   * ${photos_files}"
