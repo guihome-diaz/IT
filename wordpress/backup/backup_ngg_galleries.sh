1d1df4a85941cfb9f7e8eb9bac7a46d93b721f3d
Guillaume <guillaume@qin-diaz.com>
2020-08-30 18:07:23 +0200

    List photos files

--- wordpress/backup/backup_ngg_galleries.sh
+++ wordpress/backup/backup_ngg_galleries.sh
@@ -13,6 +13,7 @@
 ##### Global variables (they are populated by the functions)
 declare -a galleries
 declare -a photos_files
+photos_index=0
 
 # Get current IP @, do not change that line
 WORDPRESS_ROOT=$(pwd)
@@ -47,11 +48,12 @@ function getPhotosFiles() {
 
 
   # bash v4.4+
-  #mapfile -d $'\0' photos_files < <(find ${WORDPRESS_ROOT}/wp-content/gallery/ -name "*.*_backup" -print0)
-
-  # old bash (OVH server is v4.14 in 2020/09)
-  #  readarray -t photos_files < <(find ${WORDPRESS_ROOT}/wp-content/gallery/ -name "*.*_backup")
-  readarray -t photos_files < <(find "${WORDPRESS_ROOT}/wp-content/gallery/" -name "*.*_backup" -type f)
+  find_photos=(find "${WORDPRESS_ROOT}/wp-content/gallery/" -name "*.*_backup" -type f)
+  for photo_file in ${find_photos}; do
+    photos_files[${photos_index}]="${photo_file}"
+    photos_index=$(( photos_index + 1 ))
+    echo -e "   * ${photo_file}"
+  done
 
   echo -e "    ${YELLOW}${#photos_files[@]} photos found${WHITE}"
   echo -e " "
